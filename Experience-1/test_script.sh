5fef05dde5f8415823e6c34cdaf01c55d23b0e96
Yorkson <434998827@qq.com>
2017-09-15 04:17:32 +0800

    add script

---
+++ Experience-1/test_script.sh
@@ -0,0 +1,220 @@
+#!/bin/bash
+echo "script start..."
+
+# n = 10000
+for k in $( seq 1 20)
+do
+    ./main 1 10000
+done
+for k in $( seq 1 20)
+do
+    ./main 2 10000
+done
+for k in $( seq 1 20)
+do
+    ./main 3 10000
+done
+for k in $( seq 1 20)
+do
+    ./main 4 10000
+done
+for k in $( seq 1 20)
+do
+    ./main 5 10000
+done
+# n = 20000
+for k in $( seq 1 20)
+do
+    ./main 1 20000
+done
+for k in $( seq 1 20)
+do
+    ./main 2 20000
+done
+for k in $( seq 1 20)
+do
+    ./main 3 20000
+done
+for k in $( seq 1 20)
+do
+    ./main 4 20000
+done
+for k in $( seq 1 20)
+do
+    ./main 5 20000
+done
+# n = 30000
+for k in $( seq 1 20)
+do
+    ./main 1 30000
+done
+for k in $( seq 1 20)
+do
+    ./main 2 30000
+done
+for k in $( seq 1 20)
+do
+    ./main 3 30000
+done
+for k in $( seq 1 20)
+do
+    ./main 4 30000
+done
+for k in $( seq 1 20)
+do
+    ./main 5 30000
+done
+# n = 40000
+for k in $( seq 1 20)
+do
+    ./main 1 40000
+done
+for k in $( seq 1 20)
+do
+    ./main 2 40000
+done
+for k in $( seq 1 20)
+do
+    ./main 3 40000
+done
+for k in $( seq 1 20)
+do
+    ./main 4 40000
+done
+for k in $( seq 1 20)
+do
+    ./main 5 40000
+done
+# n = 50000
+for k in $( seq 1 20)
+do
+    ./main 1 50000
+done
+for k in $( seq 1 20)
+do
+    ./main 2 50000
+done
+for k in $( seq 1 20)
+do
+    ./main 3 50000
+done
+for k in $( seq 1 20)
+do
+    ./main 4 50000
+done
+for k in $( seq 1 20)
+do
+    ./main 5 50000
+done
+
+# n = 100
+for k in $( seq 1 20)
+do
+    ./main 1 100
+done
+for k in $( seq 1 20)
+do
+    ./main 2 100
+done
+for k in $( seq 1 20)
+do
+    ./main 3 100
+done
+for k in $( seq 1 20)
+do
+    ./main 4 100
+done
+for k in $( seq 1 20)
+do
+    ./main 5 100
+done
+
+# n = 1000
+for k in $( seq 1 20)
+do
+    ./main 1 1000
+done
+for k in $( seq 1 20)
+do
+    ./main 2 1000
+done
+for k in $( seq 1 20)
+do
+    ./main 3 1000
+done
+for k in $( seq 1 20)
+do
+    ./main 4 1000
+done
+for k in $( seq 1 20)
+do
+    ./main 5 1000
+done
+
+# n = 10000
+for k in $( seq 1 20)
+do
+    ./main 1 10000
+done
+for k in $( seq 1 20)
+do
+    ./main 2 10000
+done
+for k in $( seq 1 20)
+do
+    ./main 3 10000
+done
+for k in $( seq 1 20)
+do
+    ./main 4 10000
+done
+for k in $( seq 1 20)
+do
+    ./main 5 10000
+done
+
+# n = 100000
+for k in $( seq 1 20)
+do
+    ./main 1 100000
+done
+for k in $( seq 1 20)
+do
+    ./main 2 100000
+done
+for k in $( seq 1 20)
+do
+    ./main 3 100000
+done
+for k in $( seq 1 20)
+do
+    ./main 4 100000
+done
+for k in $( seq 1 20)
+do
+    ./main 5 100000
+done
+
+# n = 1000000
+for k in $( seq 1 20)
+do
+    ./main 1 1000000
+done
+for k in $( seq 1 20)
+do
+    ./main 2 1000000
+done
+for k in $( seq 1 20)
+do
+    ./main 3 1000000
+done
+for k in $( seq 1 20)
+do
+    ./main 4 1000000
+done
+for k in $( seq 1 20)
+do
+    ./main 5 1000000
+done
+
+echo "script finished."
