9ab5882442cfa79920396bbd6eeb2bb6288a0675
Yorkson <434998827@qq.com>
2017-09-15 11:10:32 +0800

    update

--- Experience-1/main.cpp
+++ Experience-1/main.cpp
@@ -24,46 +24,47 @@ int main(int argc, char *argv[])
         a[i] = rand() % 100000;
         b[i] = 0;
     }
-
+    // 所选方法
+    int sorting_method = argv[1][0] - '0';
     // 计时开始
     long start = clock();
 
-    int sorting_method = argv[1][0] - '0';
-    switch (sorting_method)
-    {
-    case 1:
-        cout << "Selection sorting: " << endl;
-        sort.selectionSort(a, SIZE);
-        break;
-    case 2:
-        cout << "Bubble sorting: " << endl;
-        sort.bubble(a, SIZE);
-        break;
-    case 3:
-        cout << "Merge sorting: " << endl;
-        sort.mergeSort(a, b, 0, SIZE - 1);
-        break;
-    case 4:
-        cout << "Quick sorting:" << endl;
-        sort.quickSort(a, 0, SIZE - 1);
-        break;
-    case 5:
-        cout << "Insert sorting:" << endl;
-        sort.insertSort(a, SIZE);
-        break;
+        
+        switch (sorting_method)
+        {
+        case 1:
+            cout << "Selection sorting: " << endl;
+            sort.selectionSort(a, SIZE);
+            break;
+        case 2:
+            cout << "Bubble sorting: " << endl;
+            sort.bubble(a, SIZE);
+            break;
+        case 3:
+            cout << "Merge sorting: " << endl;
+            sort.mergeSort(a, b, 0, SIZE - 1);
+            break;
+        case 4:
+            cout << "Quick sorting:" << endl;
+            sort.quickSort(a, 0, SIZE - 1);
+            break;
+        case 5:
+            cout << "Insert sorting:" << endl;
+            sort.insertSort(a, SIZE);
+            break;
 
-    default:
-        cout << "ERROR." << endl;
-        return -1;
-    }
+        default:
+            cout << "ERROR." << endl;
+            return -1;
+        }
 
     // 计时结束
     long finish = clock();
 
     double totaltime = (double)(finish - start) / CLOCKS_PER_SEC; // 算法耗时计算
-    for (int i = 0; i < SIZE; i++)
-        cout << a[i] << "  ";
-    cout << endl;
+    // for (int i = 0; i < SIZE; i++)
+    //     cout << a[i] << "  ";
+    // cout << endl;
     cout << "Data size: " << SIZE << ' ' << "Time cost：" << totaltime << 's' << endl;
     cout << endl
          << endl;
